87aa00e19aeec13e0cf20bc4ceec5db76241b7fb
hrachishkhanyan <hrachishkhanyan@gmail.com>
2021-09-17 16:35:37 +0400

    Homework day 4 checked

---
+++ Homework_1_checked.py
@@ -0,0 +1,44 @@
+# 1. Create a 3x3 matrix that will contain the squares of the numbers from 1-9 using a nested loop.
+# Օգտագործելով ցիկլեր, ստեղծել 3x3 մատրից, որը կպարունակի 1-9 թվերի քառակուսիները։
+
+a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+b = [[x ** 2 for x in row] for row in a]
+print(b)
+
+# 2. Create a 3x3 matrix that will contain the squares of the numbers from 1-9 using a list comprehension.
+# Օգտագործելով comprehension, ստեղծել 3x3 մատրից, որը կպարունակի 1-9 թվերի քառակուսիները։
+
+# a = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+# for i in range(3):
+#     for j in range(3):
+#         a[i][j] **= 2
+#         print(a[i][j], end=" ")
+#     print()
+
+# Երկու տարբերակն էլ լավ էր։ Բայց մի խնդիր կա, պատկերացրու 100x100 մատրիցի հետ գործ ունես ու պետք ա նույն գործողությունը
+# անես։ Հաստատ ամենահարմար տարբերակը չի ձեռքով ստեղծել էդ մատրիցը :) Էնպես որ փորձիր a-ն ստեղծել ցիկլի միջոցով։
+
+# 4. Count the number of 'b's in the given string. DO NOT use the count() method.
+# Հաշվել տրված սթրինգում 'b'-երի քանակը։ Չօգտագործել count() մեթոդը։
+
+nonsense = 'Blinking blindly, brainy Bob brings beautiful beer bottles beneath blue butterflies billowing brilliantly.'
+b = 0
+for i in nonsense:
+    if i == 'b':  # Իսկ փոքրատառերը՞
+        b += 1
+print(f'count b is {b}')
+
+
+
+# 5. Write a program that will print the factorial of n.
+# Գրել ծրագիր, որը կտպի n թվի ֆակտորիալը։
+
+n = int(input("Enter your factorial number: "))
+result = 1
+for i in range(2, n+1):
+    result *= i
+print(result)
